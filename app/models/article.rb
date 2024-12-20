d4db5c533b47b2f504aa8c0501bf13e0ff672b98
Amanda Rangel <amandarangelaraujo@gmail.com>
2024-12-20 14:31:45 -0300

    Added photos to articles

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,5 @@
 class Article < ApplicationRecord
   belongs_to :user
   has_many :comments, dependent: :destroy #para ele apagar os comentários quando article for excluido
-  
+  has_one_attached :photo
 end
